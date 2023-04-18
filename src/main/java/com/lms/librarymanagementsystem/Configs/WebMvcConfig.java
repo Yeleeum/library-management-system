205427f32529034c6e843df5c384d28d4ce46fb9
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-04-18 16:06:43 +0530

    User Route Restriced

--- src/main/java/com/lms/librarymanagementsystem/Configs/WebMvcConfig.java
+++ src/main/java/com/lms/librarymanagementsystem/Configs/WebMvcConfig.java
@@ -6,15 +6,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.lms.librarymanagementsystem.Interceptors.AdminInterceptor;
 import com.lms.librarymanagementsystem.Interceptors.LoginInterceptor;
+import com.lms.librarymanagementsystem.Interceptors.UserInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     private LoginInterceptor loginInterceptor;
     private AdminInterceptor adminInterceptor;
+    private UserInterceptor userInterceptor;
 
-    public WebMvcConfig(LoginInterceptor loginInterceptor,AdminInterceptor adminInterceptor) {
+    public WebMvcConfig(LoginInterceptor loginInterceptor,AdminInterceptor adminInterceptor,UserInterceptor userInterceptor) {
         this.loginInterceptor = loginInterceptor;
         this.adminInterceptor = adminInterceptor;
+        this.userInterceptor = userInterceptor;
     }
 
     @Override
@@ -23,5 +26,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/login/**","/registration/**");
         registry.addInterceptor(adminInterceptor)
                 .addPathPatterns("/admin/**");
+        registry.addInterceptor(userInterceptor)
+                .addPathPatterns("/user/**");
     }
 }
