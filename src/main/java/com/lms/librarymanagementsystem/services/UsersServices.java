6ade36da1a8fb2416bbad80bd884560ceb8ecc49
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-04-19 22:36:32 +0530

    Uneccessary Code Removed/Commented

--- src/main/java/com/lms/librarymanagementsystem/services/UsersServices.java
+++ src/main/java/com/lms/librarymanagementsystem/services/UsersServices.java
@@ -2,9 +2,6 @@ package com.lms.librarymanagementsystem.services;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
